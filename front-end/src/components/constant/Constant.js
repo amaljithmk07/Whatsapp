d80e15e56a20f035a1db91055af3326962411689
amaljithmk07 <amaljithmk123@gmail.com>
2024-05-20 13:11:11 +0530

    base url

--- front-end/src/components/constant/Constant.js
+++ front-end/src/components/constant/Constant.js
@@ -1,4 +1,4 @@
-// const BASE_URI = `https://whatsapp-3yfa.onrender.com`;
-const BASE_URI = `https://localhost:2222`;
+const BASE_URI = `https://whatsapp-3yfa.onrender.com`;
+// const BASE_URI = `https://localhost:2222`;
 
-// export default BASE_URI;
+export default BASE_URI;
